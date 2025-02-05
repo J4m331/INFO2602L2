42b0d8f367e2972a269a40d74f7c42643ac9be10
Jameel Ali <J1m2e3A4i@gmail.com>
2025-02-04 20:47:05 -0400

    8-10

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import click, sys
-from models import db, User
+from models import db, User, Todo
 from app import app
 from sqlalchemy.exc import IntegrityError
 
@@ -8,6 +8,9 @@ def initialize():
   db.drop_all()
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
+
+  bob.todos.append(Todo('wash car'))
+
   db.session.add(bob)
   db.session.commit()
   print(bob)
@@ -65,3 +68,60 @@ def delete_user(username):
   db.session.delete(user)
   db.session.commit()
   print(f'{username} was deleted')
+
+@app.cli.command("get-todos")
+@click.argument('username', default='bob')
+def get_user_todos(username):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found')
+    return
+  print(user.todos)
+
+@app.cli.command("add-todo")
+@click.argument('username', default='bob')
+@click.argument('text', default='wash car')
+def add_task(username, text):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found')
+    return
+  newTodo = Todo(text)
+  user.todos.append(newTodo)
+  db.session.add(user)
+  db.session.commit()
+
+@app.cli.command("toggle-todo")
+@click.argument('username', default='bob')
+@click.argument('todo_id', default=1)
+def toggle_todo_command(todo_id, username):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found')
+    return
+  
+  todo = Todo.query.filter_by(id=todo_id,userId=user.id).first()
+  if not todo:
+    print(f'{username} has no todo id {todo_id}')
+    return
+
+  todo.toggle()
+  print(f'{todo.text} is {"done" if todo.done else "not done"}!')
+
+
+@app.cli.command("add-category", help="Adds a category to a todo")
+@click.argument('username', default='bob')
+@click.argument('todo_id', default=6)
+@click.argument('category', default='chores')
+def add_todo_category_command(username, todo_id, category):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found')
+    return
+
+  res = user.add_todo_category(todo_id, category)
+  if not res:
+    print(f'{username} has no todo id {todo_id}')
+    return
+
+  print('Category added!')
